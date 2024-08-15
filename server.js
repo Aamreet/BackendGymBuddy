1e663f109a03914fa83d6ae5bb8ee6757e7cf4b0
Aamreet <amreetsingh508@gmail.com>
2024-08-15 20:50:05 +0530

    install cors

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const db = require("./db");
 const express = require("express");
 const workoutRoutes = require("./routes/workoutRoutes");
 const userRoutes = require("./routes/userRoutes");
+const cors =require("cors");
+
 
 dotenv.config();
+app.use(cors());
 const app = express();
 app.use("/api/user", userRoutes);
 app.use("/api/workouts", workoutRoutes);
